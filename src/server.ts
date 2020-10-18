d2d079f61d755221b173aed3d6aef31aa1ce08fa
Thomas Chang <thomascsd808@gmail.com>
2020-10-18 20:29:23 +0800

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import * as express from 'express';
+import { useExpressServer, useContainer } from 'routing-controllers';
+import { Container } from 'typedi';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+
+export default class Server {
+  public app: express.Application;
+
+  constructor() {
+    useContainer(Container);
+    this.app = express();
+    this.config();
+    this.route();
+    this.setControllers();
+  }
+
+  public config() {
+    this.app.use(logger('combined'));
+    this.app.use(helmet());
+  }
+
+  public route() {
+    this.app.get('*', (req, res, next) => {});
+  }
+
+  public setControllers() {
+    useExpressServer(this.app, {
+      routePrefix: 'api',
+      controllers: [ContactController],
+    });
+  }
+
+  public run(port: number) {
+    this.app.listen(port, () => {
+      console.log(`App run in Port: ${port}`);
+    });
+  }
+}
