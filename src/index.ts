c7da3748c3e28e106f3f3bb4403e393970a24f53
Thomas Chang <thomascsd808@gmail.com>
2022-01-17 00:08:40 +0800

    feat: add fastify-decorator and controller

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 require('dotenv').config({ path: __dirname + '/.env' });
 import 'reflect-metadata';
-import Server from './server';
-const server = new Server();
 const port: number = parseInt(process.env.PORT, 10) || 3000;
-server.run(port);
+
+import Fastify from 'fastify';
+import { bootstrap } from 'fastify-decorators';
+import { resolve } from 'path';
+
+let server = Fastify({
+  logger: true,
+});
+
+server.register(bootstrap, {
+  directory: resolve(__dirname, 'controllers'),
+  mask: /\.controller\./,
+});
+
+server.listen(port, (error, address) => {
+  if (error) {
+    console.error(error);
+    process.exit(-1);
+  }
+
+  console.log(`Server listening at ${address}`);
+});
