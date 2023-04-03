eb512ed68428689b1c369160045724262587bc01
Thomas Chang <thomascsd808@gmail.com>
2023-04-03 17:10:23 +0800

    add getPictureOfDay

--- src/services/nasa/NasaService.ts
+++ src/services/nasa/NasaService.ts
@@ -1,9 +1,21 @@
-import { Service } from "@tsed/di";
+import { Service, Value } from '@tsed/di';
+import axios from 'axios';
+import { NasaPicture } from '../../models/nasa/NasaPicture';
 
 @Service()
-export class NasaService{
+export class NasaService {
+  @Value('NASA_API')
+  apiKey!: string;
 
+  async getPictureOfDay() {
+    const root = 'https://api.nasa.gov/planetary/apod';
+    const url = `${root}?count=10&api_key=${this.apiKey}`;
+    const res = await axios.get<NasaPicture[]>(url);
 
-    
+    return res.data;
+  }
 
+  getSpacePictures() {
+    const root = '';
+  }
 }
